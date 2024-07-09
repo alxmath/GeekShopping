0f394a394fcc720bc764cfb8e3c13e6c88a38b4f
Alex Santos <alxmath@gmail.com>
2024-07-09 14:55:25 -0300

    Criação dos VO's do Microsserviço de Produtos

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo() {Title = "GeekShopping.ProductAPI", Version = "v1"});
+});
 
 var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
