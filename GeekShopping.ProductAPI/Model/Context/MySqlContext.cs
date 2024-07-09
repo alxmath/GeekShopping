7fe5c308aae032ff03aed3bda3eaf4586c1796e4
Alex Santos <alxmath@gmail.com>
2024-07-09 14:10:20 -0300

    Mapeando as Entidades do Banco de Dados com Entity Framework

--- GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -7,4 +7,6 @@ public class MySqlContext : DbContext
     public MySqlContext() {}
     
     public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) {}
+
+    public DbSet<Product> Products { get; set; }
 }
